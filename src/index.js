83834c633e652171cc8654449ef5fda8c926bd10
Quyson Nguyen <Quyson.nguyen.1999@gmail.com>
2022-12-10 22:48:44 -0800

    Create initializeships function

--- src/index.js
+++ src/index.js
@@ -16,5 +16,25 @@ class Ship{
     }
 }
 
+function initializeShips(){
+    const shipInfo = {
+        carrier: 5,
+        battleship: 4,
+        cruiser: 3,
+        submarine: 3,
+        destroyer: 2,
+        scout: 2
+    }
+    let shipList = []
+    for(item in shipInfo){
+        let ship = new Ship(item, shipInfo[item]);
+        shipList.push(ship)
+    }
+    return shipList;
+}
+let player1 = new Player("one");
+player1.ships = initializeShips();
+console.log(player1.ships)
+
 
 
